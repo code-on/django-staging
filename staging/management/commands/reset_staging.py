0f566446b66c5cb510b8057e415a63f390ab14df
vitaliy <ppr.vitaly@gmail.com>
2011-12-07 07:00:30 +0200

    migrate-all only if south in installed apps

--- staging/management/commands/reset_staging.py
+++ staging/management/commands/reset_staging.py
@@ -11,6 +11,11 @@ class Command(BaseCommand):
             if os.path.exists(db_file):
                 if not 'nodelete' in args:
                     os.unlink(db_file)
-            call_command('syncdb', database=db_key, interactive=False, migrate_all=True)
+            
+            kwargs = dict(database=db_key, interactive=False)
+            if 'south' in settings.INSTALLED_APPS:
+                kwargs['migrate_all'] = True
+            
+            call_command('syncdb', **kwargs)
         call_command('load_staging')
-        #call_command('rebuild_index', interactive=False)
+
