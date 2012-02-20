fbb2633a20d41562fc46eddbfd164027f020b026
Sergey Russkih <srusskih@code-on.be>
2012-02-20 10:10:25 +0200

    add setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='django-staging',
+    version='0.1',
+    packages=['staging', 'staging.management', 'staging.management.commands'],
+    url='https://github.com/pydevua/django-staging',
+    license='BSD licence, see LICENCE',
+    description='Project staging helpers',
+    long_description=open('README.md').read()
+)
