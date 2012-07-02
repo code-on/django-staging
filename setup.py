20f9ebe8ab1719e706a78efefff1c6f206cd431a
srusskih <smilemouse2002@ukr.net>
2012-07-02 15:47:18 +0300

    set correct project URL in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='django-staging',
     version='0.1',
     packages=['staging', 'staging.management', 'staging.management.commands'],
-    url='https://github.com/pydevua/django-staging',
+    url='https://github.com/code-on/django-staging',
     license='BSD licence, see LICENCE',
     description='Project staging helpers',
     long_description=open('README.md').read()
